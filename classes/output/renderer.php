9efe098eb4de6921bed55b5ea45b60a41fdbde10
Ajit Kumar <ajitkumar22@gmail.com>
2025-06-10 07:47:49 +0000

    count votes added

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -8,14 +8,21 @@ use plugin_renderer_base;
 
 class renderer extends plugin_renderer_base {
     public function render_hello_message(): string {
-        global $USER, $COURSE, $PAGE;
+        global $DB, $USER, $COURSE, $PAGE;
+
+        $pageurl = $PAGE->url->out();
+        // Count votes for this page
+        $likes = $DB->count_records('block_likes_votes', ['pageurl' => $pageurl, 'vote' => 'like']);
+        $dislikes = $DB->count_records('block_likes_votes', ['pageurl' => $pageurl, 'vote' => 'dislike']);
 
         $data = [
             'likeurl' => new \moodle_url('/blocks/likes/like.php'),
             'userid' => $USER->id,
             'courseid' => $COURSE->id ?? 1,
-            'pageurl' => $PAGE->url->out(),
-            'sesskey' => sesskey()
+            'pageurl' => $pageurl,
+            'sesskey' => sesskey(),
+             'likecount' => $likes,
+            'dislikecount' => $dislikes
         ];
         return $this->render_from_template('block_likes/hello_message', $data);
     }
